6c2dd95c0f7fbc7787031c6f4d8e0fe1207769be
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-06-08 00:20:38 -0500

    asdas

--- ShootterSuprem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.shoottersuprem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.shoottersuprem to javafx.fxml;
-    exports com.example.shoottersuprem;
-}
