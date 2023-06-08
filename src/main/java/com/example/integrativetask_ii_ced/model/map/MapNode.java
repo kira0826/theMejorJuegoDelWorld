94e07698fd5f4d36aecf0d6e1371d50e8d1d4319
Ricardo965 <ricardiox2000@gmail.com>
2023-06-08 03:34:56 -0500

    Verify mapnode collision with bullet in run method

--- src/main/java/com/example/integrativetask_ii_ced/model/map/MapNode.java
+++ src/main/java/com/example/integrativetask_ii_ced/model/map/MapNode.java
@@ -1,15 +1,18 @@
 package com.example.integrativetask_ii_ced.model.map;
 
 import com.example.integrativetask_ii_ced.model.drawing.Drawable;
+import com.example.integrativetask_ii_ced.model.drawing.HelloController;
 import com.example.integrativetask_ii_ced.model.entities.objects.Obstacle;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
-public class MapNode extends Obstacle implements Drawable {
+public class MapNode extends Obstacle implements Drawable , Runnable{
 
     boolean navigable;
     private Image[] design;
+
+    private  Boolean isDestructable;
 
 
     public MapNode(double x, double y, boolean navigable) {
@@ -46,5 +49,19 @@ public class MapNode extends Obstacle implements Drawable {
 
     public void setDesign(Image[] design) {
         this.design = design;
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            if(this.isDestructable){
+                for (int i = 0; i < HelloController.levels.get(0).getBullets().size(); i++) {
+                    if (HelloController.levels.get(0).getBullets().get(i).getHitBox().comparePosition(this.hitBox)){
+                        HelloController.levels.get(0).getBullets().remove(i);
+                        setNavigable(true);
+                    }
+                }
+            }
+        }
     }
 }
